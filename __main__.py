2ecb75641b1471d9fbd51ff93bca18a0ebe78ee0
Alexander Simpson <alexsimpson440dev@gmail.com>
2017-09-06 23:37:36 -0500

    Added add_new_table method that adds a table, also added a close_connection method to close the connection

--- __main__.py
+++ __main__.py
@@ -4,6 +4,8 @@ def main():
     database = SQL
     database.__init__(database)
     database.get_connection(database)
+    database.add_new_table(database)
+    database.close_connection(database)
 
 if __name__ == '__main__':
     main()
--- src/SQLlite.py
+++ src/SQLlite.py
@@ -3,8 +3,21 @@ import sqlite3
 class SQL():
     def __init__(self):
         self.sqlite_file = 'F:\School\MCTC\Classes\Final Semester\Capstone\Week3 - SQLite\my_db.sqlite'
+        #Table(s)
+        self.employees_table = "Employees"
+        #Employees table fields
+        self.employeeID_field = "EmployeeID"; self.INTEGER = 'INTEGER'
+        self.employeename_field = "EmployeeName"; self.TEXT = "TEXT"
+        self.employeehire_field = "Hire Date" #uses type2
+        self.employeejob = "Job Description" #uses type2
 
     def get_connection(self):
         self.conn = sqlite3.connect(self.sqlite_file)
         self.c = self.conn.cursor()
 
+    def add_new_table(self):
+        self.c.execute('CREATE TABLE {tn} ({nc} {nf})'.format(tn=self.employees_table, nc = self.employeeID_field, nf=self.INTEGER))
+
+    def close_connection(self):
+        self.conn.commit()
+        self.conn.close()
